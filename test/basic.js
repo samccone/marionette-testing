acaeb1ee2cbb6e65990d7333bace99ef6677e017
Sam Saccone <samccone@gmail.com>
2015-06-26 14:34:10 -0700

    :lipstick: Update to match jscs

--- test/basic.js
+++ test/basic.js
@@ -1,42 +1,41 @@
 chai = require('chai');
 chai.should();
 
-jsdom = require("jsdom").jsdom;
-global.document = jsdom("<html></html>");
+jsdom = require('jsdom').jsdom;
+global.document = jsdom('<html></html>');
 global.window = global.document.parentWindow;
-_ = require("lodash");
-backbone = require("backbone");
-Mn = require("backbone.marionette");
+_ = require('lodash');
+backbone = require('backbone');
+Mn = require('backbone.marionette');
 
-describe("rendering", function() {
+describe('rendering', function() {
   beforeEach(function() {
-    this.person = new backbone.Model({name: "ryan lost his phone"});
+    this.person = new backbone.Model({name: 'ryan lost his phone'});
 
     this.rendered = new Mn.ItemView({
-      template: _.template("hi <%= name %>"),
+      template: _.template('hi <%= name %>'),
       model: this.person,
       modelEvents: {
         'change': 'render'
       },
       events: {
-        "click": function() {
-            this.model.set('name', 'tomtom');
+        'click': function() {
+          this.model.set('name', 'tomtom');
         }
       }
     }).render();
   });
 
-  it("should render", function() {
+  it('should render', function() {
     this.rendered.el.innerHTML.should.eql('hi ryan lost his phone');
   });
 
-  it("should change name on click", function() {
+  it('should change name on click', function() {
     this.rendered.$el.click();
     this.rendered.el.innerHTML.should.eql('hi tomtom');
   });
 
-
-  it("should change name when the model is updated", function() {
+  it('should change name when the model is updated', function() {
     this.person.set('name', 'foo');
     this.rendered.el.innerHTML.should.eql('hi foo');
   });
