8c43998ae62f10eff18bc6f810379d22ff9c39a6
Sam Saccone <samccone@gmail.com>
2015-06-26 14:22:37 -0700

    :goat: Add more tests

--- test/basic.js
+++ test/basic.js
@@ -1,5 +1,6 @@
 chai = require('chai');
 chai.should();
+
 jsdom = require("jsdom").jsdom
 global.document = jsdom("<html></html>")
 global.window = global.document.parentWindow
@@ -10,13 +11,38 @@ backbone = require("backbone")
 backbone.$ = jquery
 Mn = require("backbone.marionette")
 
-
 describe("rendering", function() {
-  it("should render", function() {
-    var rendered = new Mn.ItemView({
-      template: _.template("hi")
+  beforeEach(function() {
+    this.person = new backbone.Model({name: "ryan lost his phone"});
+
+    this.rendered = new Mn.ItemView({
+      template: _.template("hi <%= name %>"),
+      model: this.person,
+      modelEvents: {
+        'change': 'render'
+      },
+      events: {
+        "click": function() {
+            this.model.set('name', 'tomtom')
+        }
+      }
     }).render();
 
-    rendered.el.innerHTML.should.eql('hi');
+  })
+
+  it("should render", function() {
+    this.rendered.el.innerHTML.should.eql('hi ryan lost his phone');
+  });
+
+  it("should change name on click", function() {
+    this.rendered.$el.click();
+    this.rendered.el.innerHTML.should.eql('hi tomtom');
+  });
+
+
+  it("should change name when the model is updated", function() {
+    this.person.set('name', 'foo');
+    this.rendered.el.innerHTML.should.eql('hi foo');
   });
 })
+
